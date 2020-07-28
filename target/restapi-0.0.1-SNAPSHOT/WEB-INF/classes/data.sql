fc26a20e8b0b4d9502cfd9afb812f2c6e4e55ccd
Braian Varona <braianv@unitech.com.ar>
2020-07-28 16:58:16 -0300

    Initial commit

---
+++ target/restapi-0.0.1-SNAPSHOT/WEB-INF/classes/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO USER (email,first_name,last_name) values('pepe.argento@gmail.com','Pepe','Argento' );
+INSERT INTO USER (email,first_name,last_name) values('moni_argento@gmail.com','Moni', 'Argento');
+INSERT INTO USER (email,first_name,last_name) values('mefuseneco@gmail.com','Maria Elena', 'Fuseneco');
+INSERT INTO USER (email,first_name,last_name) values('dardo-fuseneco@gmail.com', 'Dardo', 'fuseneco');
+
+INSERT INTO LOAN (total,user_id) values(50000, 1);
+INSERT INTO LOAN (total,user_id) values(25000, 1);
+INSERT INTO LOAN (total,user_id) values(12500, 2);
+INSERT INTO LOAN (total,user_id) values(10000, 3);
+
+
+
